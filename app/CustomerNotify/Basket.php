bf3931e4915dde2796e34a59f4685a227d9e26c3
Nikolay <swede198@ya.ru>
2019-08-08 18:57:57 +0500

    del index and two

--- app/CustomerNotify/Basket.php
+++ app/CustomerNotify/Basket.php
@@ -6,18 +6,35 @@ require_once(__DIR__ . "\Product.php");
 
 class Basket
 {
-    public function addProduct($product, $quantity, $price)
+    public static function addProduct($product, $quantity, $price)
     {
+        $basket = [];
+        $basket[] = array(
+            "NAME" => $product,
+            "QNT" => $quantity,
+            "PRICE" => $price
+        );
 
+        self::getPrice($basket);
+//        self::describe($product, $quantity, $price);
     }
 
-    public function getPrice()
+    public function getPrice($basket)
     {
-
+        $totalPriceAr = [];
+        foreach ($basket as $key => $product) {
+            $price = $product["PRICE"];
+            $qnt = $product["QNT"];
+            $oneProductPrice = $price * $qnt;
+            $totalPriceAr[] = $oneProductPrice;
+        }
+//        var_dump($totalPriceAr[0]);
+        $totalPrice = array_sum($totalPriceAr);
+        echo $totalPrice;
     }
 
-    public function describe()
+    public function describe($product, $quantity, $price)
     {
-
+        echo $product . " — " . $price . " — " . $quantity . "<br />";
     }
 }
--- app/CustomerNotify/index.php
+++ app/CustomerNotify/index.php
@@ -6,5 +6,12 @@ require_once(__DIR__ . "\Basket.php");
 require_once(__DIR__ . "\Product.php");
 
 $firstProduct = new Product("Test", "15000");
+$secondProduct = new Product("Второй товар", "10000");
+$thirdProduct = new Product("Третий товар", "11000");
+$fourthProduct = new Product("Четвертый товар", "12000");
+$fifthProduct = new Product("Пятый товар", "10000");
+$sixthProduct = new Product("Шестой товар", "10000");
+$seventhProduct = new Product("Седьмой товар", "9000");
 
-var_dump($firstProduct);
+Basket::addProduct($firstProduct->name, "2", $firstProduct->price);
+Basket::addProduct($thirdProduct->name, "1", $thirdProduct->price);
