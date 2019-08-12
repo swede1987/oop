e93068b7a93d347ebbd4ec6033cb542a1dd3ea26
Nikolay <swede198@ya.ru>
2019-08-12 11:23:04 +0500

    add say and walk

--- app/Farm/Farm.php
+++ app/Farm/Farm.php
@@ -12,20 +12,37 @@ class Farm
 
 class Animals
 {
+    public function say()
+    {
 
+    }
+
+    public function walk()
+    {
+        echo "Топ-топ";
+    }
 }
 
 class Pig extends Animals
 {
-
+    public function say()
+    {
+        echo "Хрю-хрю";
+    }
 }
 
 class Cow extends Animals
 {
-
+    public function say()
+    {
+        echo "Му-му-му";
+    }
 }
 
 class Chicken extends Animals
 {
-
+    public function say()
+    {
+        echo "Ко-ко-ко";
+    }
 }
