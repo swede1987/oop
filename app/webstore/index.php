74ec5131138240fd6f4ebdc7d04aa51be9a16c62
Nikolay <swede198@ya.ru>
2019-08-13 18:49:10 +0500

    order

--- app/webstore/index.php
+++ app/webstore/index.php
@@ -24,10 +24,13 @@ $basket1->addProduct(new Product("Третий продукт", 3500), "4", 3);
 $firstOrder = new Order($basket1);
 $order = $firstOrder->getBasket();
 
-foreach ($order as $key => $item) {
-    $item .= $item;
-}
+//foreach ($order as $key => $item) {
+//
+//    $item .= $item;
+//
+//    echo $item . "<br/>";
+//}
 
 
 $firstClient = new \App\CustomerNotify\User("Николай Николаевич", "3@ya.ru", null, "35", null);
-$firstClient->notifyOnEmail("для вас создан заказ, на сумму:" . $firstOrder->getPrice() . " руб.<br/> Состав:<br/>" . $item);
+$firstClient->notifyOnEmail("для вас создан заказ, на сумму:" . $firstOrder->getPrice() . " руб.<br/> Состав:<br/>" . implode("", $order));
